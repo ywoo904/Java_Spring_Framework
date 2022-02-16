7860d1e5c59721af7b82ea5a03f677cba642fd5b
ywoo904 <ywoo904@gmail.com>
2022-02-16 15:39:30 +0900

    Spring_MemberVO
    Spring_MemberVO

---
+++ SpringBasic/src/main/java/com/zerock/command/MemberVO.java
@@ -0,0 +1,47 @@
+package com.zerock.command;
+
+public class MemberVO {
+
+	//기본 생성자
+	String id; 
+	String pw; 
+	String name; 
+	
+	public MemberVO()  { }
+
+	public MemberVO(String id, String pw, String name) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	} 
+	
+	
+	
+	
+	
+}
